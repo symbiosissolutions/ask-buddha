da68e970883055a343910aa012a4087a8d1fd0ce
Aatish Dubey <atsh_dubey@live.com>
2024-05-19 23:33:38 +0545

    initial proof of concept
    Setup openai client with methods:
    - `createThread`
    - `createMessageInThread`
    - `createRun`
    - `listMessages`
    The combination of these four methods results in
    the retrieval of a response for the user's query
    from the chat assistant.
    
    Other changes:
    - Also setup prettier for formatting since things
    were getting messy
    - Created a bare minimum chat UI for now
    - Added supplementary package `uuid`

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useEffect, useState } from "react";
+import "./App.css";
+import { openaiClient } from "./openai/threads";
+import { API_KEY, ASSISTANT_ID } from "./constants/config";
+import { ROLES } from "./constants/enums";
+import { TextContentBlock } from "openai/resources/beta/threads/messages.mjs";
+import { v4 as uuidv4 } from "uuid";
+
+type TMessage = {
+  id: string;
+  role: `${ROLES}`;
+  content: string;
+};
+
+const assistant = openaiClient(API_KEY);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [threadId, setThreadId] = useState<string | undefined>();
+  const [messages, setMessages] = useState<TMessage[]>([]);
+  const [appInitializing, setAppInitializing] = useState(true);
+  const [loadingAssistantResponse, setLoadingAssistantResponse] =
+    useState(false);
+  const [userInput, setUserInput] = useState("");
+
+  const init = async () => {
+    setAppInitializing(true);
+    const thread = await assistant.createThread();
+    setThreadId(thread.id);
+    setAppInitializing(false);
+  };
+
+  useEffect(() => {
+    init();
+    return () => {
+      setThreadId(undefined);
+      setMessages([]);
+      setUserInput("");
+    };
+  }, []);
+
+  const sendMessageAndGetResponse = async (message: string) => {
+    if (threadId !== undefined) {
+      await sendAndProcess();
+      return await getResponse();
+    }
+
+    async function sendAndProcess() {
+      if (threadId !== undefined) {
+        await assistant.createMessageInThread(threadId, message);
+        await assistant.createRun(threadId, ASSISTANT_ID);
+      }
+    }
+
+    async function getResponse() {
+      if (threadId !== undefined) {
+        const allMessagesInThread = assistant.listMessages(threadId);
+        const lastResponse = (await allMessagesInThread).data[0]
+          .content[0] as TextContentBlock;
+        return lastResponse.text.value;
+      }
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMessages((currentMessages) => [
+      ...currentMessages,
+      {
+        id: uuidv4(),
+        content: userInput,
+        role: "user",
+      },
+    ]);
+    setUserInput("");
+    setLoadingAssistantResponse(true);
+    const assistantResponse = await sendMessageAndGetResponse(userInput);
+    setMessages((currentMessages) => [
+      ...currentMessages,
+      {
+        id: uuidv4(),
+        content: assistantResponse as string,
+        role: "assistant",
+      },
+    ]);
+    setLoadingAssistantResponse(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {appInitializing ? (
+        <>
+          <div>Loading...</div>
+        </>
+      ) : (
+        <>
+          <div className="chatbox">
+            {messages.map((message) => (
+              <div key={message.id} className={`message ${message.role}`}>
+                <p>{message.content}</p>
+              </div>
+            ))}
+            {loadingAssistantResponse && (
+              <div className={`message assistant`}>
+                <p>...</p>
+              </div>
+            )}
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              disabled={loadingAssistantResponse}
+              placeholder="What would you like to ask Buddha today?"
+            />
+            <button type="submit" disabled={loadingAssistantResponse}>Send</button>
+          </form>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/constants/config.ts
@@ -0,0 +1,2 @@
+export const API_KEY: string = import.meta.env.VITE_OPENAI_API_KEY;
+export const ASSISTANT_ID: string = import.meta.env.VITE_OPENAI_ASSISTANT_ID;
